9c1df811d909d6d03e8478afdd363d5c6bd80aac
Abhishek94531 <108179409+Abhishek94531@users.noreply.github.com>
2023-02-02 12:40:21 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,23 +14,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94786802371'] //ur owner number
-global.ownername = "Nimesh Official" //ur owner name
-global.ytname = "YT: HELA CLOUD" //ur yt chanel name
-global.socialm = "GitHub: nimaofficial" //ur github or insta name
-global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
+global.owner = ['919453192366'] //ur owner number
+global.ownername = "abhishek" //ur owner name
+global.ytname = "TITAN0009" //ur yt chanel name
+global.socialm = "INSTA :- @WHY_HELLO_HOOMAN" //ur github or insta name
+global.location = "lucknow,uttar pradesh,india" //ur location
 
 //bot bomdy 
-global.ownernomer = "94786802371" //ur number
-global.premium = ['94786802371'] //ur premium number
-global.botname = 'BLACK DRAGON' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://www.helacloud.ga" //ur website to be displayed
+global.ownernomer = "919453192366" //ur number
+global.premium = ['919453192366'] //ur premium number
+global.botname = 'ALBEDO' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
 global.botscript = 'https://github.com/nimaofficial/Black-Dragon' //script link
 global.themeemoji = "❤️" //ur theme emoji
-global.packname = "Sticker By Black Dragon" //ur sticker watermark packname
-global.author = "Nimesh Official" //ur sticker watermark author
-global.wm = "𝗕𝗟𝗔𝗖𝗞 𝗗𝗥𝗔𝗚𝗢𝗡 𝗜𝗡𝗖" //ur watermark
+global.packname = "Sticker By Lady Albedo" //ur sticker watermark packname
+global.author = "Abhishek" //ur sticker watermark author
+global.wm = "ALBEDO 𝗜𝗡𝗖" //ur watermark
 
 // Other
 global.sessionName = 'session'
